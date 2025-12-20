60f5d8af375da993e99bcbcc994ba694b8587aa7
bimuafaq <bimuafaq@users.noreply.github.com>
2025-12-20 07:25:56 +0700

    ( #188 ) 2025-12-20 07:25:56

--- build.sh
+++ build.sh
@@ -1,43 +1,34 @@
 #!/usr/bin/env bash
 
 setup_src() {
-    repo init -u https://github.com/LineageOS/android.git -b lineage-17.1 --depth=1
+    repo init -u https://github.com/LineageOS/android.git -b lineage-17.1
     git clone -q https://github.com/rovars/rom xx
     mkdir -p .repo/local_manifests/
     mv xx/10/lin10.xml .repo/local_manifests
-    mv xx/10/rev.xml .repo/local_manifests
+    # mv xx/10/rev.xml .repo/local_manifests
     repo sync -j8 -c --no-clone-bundle --no-tags
 }
 
 build_src() {    
-    source build/envsetup.sh   
+    source build/envsetup.sh
+
+    chmod +x xx/10/*.sh
+    source xx/10/repopick.sh
+    source xx/10/cb.sh
  
     export OWN_KEYS_DIR=$PWD/xx/keys
     sudo ln -s $OWN_KEYS_DIR/releasekey.pk8 $OWN_KEYS_DIR/testkey.pk8
     sudo ln -s $OWN_KEYS_DIR/releasekey.x509.pem $OWN_KEYS_DIR/testkey.x509.pem
 
-    brunch RMX2185
+    # brunch RMX2185
 }
 
-upload_src() {  
-    REPO="rovars/release"
-    RELEASE_TAG="lineage-17.1"
+upload_src() {
     ROM_FILE=$(find out/target/product -name "*-RMX*.zip" -print -quit)
-    ROM_X="https://github.com/$REPO/releases/download/$RELEASE_TAG/$(basename "$ROM_FILE")"
 
-    echo "$tokenpat" > tokenpat.txt
-    gh auth login --with-token < tokenpat.txt
-
-    if ! gh release view "$RELEASE_TAG" -R "$REPO" > /dev/null 2>&1; then
-        gh release create "$RELEASE_TAG" -t "$RELEASE_TAG" -R "$REPO" --generate-notes
-    fi
-
-    #gh release upload "$RELEASE_TAG" "$ROM_FILE" -R "$REPO" --clobber || true
-
-    echo "$ROM_X"
-    MSG_XC2="( <a href='https://cirrus-ci.com/task/${CIRRUS_TASK_ID}'>Cirrus CI</a> ) - $CIRRUS_COMMIT_MESSAGE ( <a href='$ROM_X'>$(basename "$CIRRUS_BRANCH")</a> )"
-    xc -s "$MSG_XC2"
+    echo "$tokenpat" | gh auth login --with-token
+    gh release view "lineage-17.1" -R "rovars/release" >/dev/null 2>&1 || gh release create "lineage-17.1" -R "rovars/release" -t "lineage-17.1" --generate-notes
 
     mkdir -p ~/.config && mv xx/config/* ~/.config
-    timeout 15m telegram-upload $ROM_FILE --to $idtl --caption "$CIRRUS_COMMIT_MESSAGE" || true
+    timeout 15m telegram-upload "$ROM_FILE" --to "$idtl" --caption "$CIRRUS_COMMIT_MESSAGE" || true
 }

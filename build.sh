6b0a5a292359e7a09f335e3860b9e901e84ae2fa
bimuafaq <bimuafaq@users.noreply.github.com>
2026-02-08 13:10:13 +0700

    (#459) 2026-02-08 13:10:13

--- build.sh
+++ build.sh
@@ -70,8 +70,8 @@ build_src() {
     sudo ln -sf "$OWN_KEYS_DIR/releasekey.x509.pem" "$OWN_KEYS_DIR/testkey.x509.pem"
 
     lunch lineage_RMX2185-user
-    # source $PWD/xx/script/m.sh system
-    mka bacon
+    source $PWD/xx/script/m.sh system || exit 1
+    # mka bacon
 }
 
 upload_src() {
